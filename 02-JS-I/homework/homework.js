10d538b6cd72c9eaca3f412972f9c2511cb8a69b
Den1Baro <denise_baroffio@hotmail.comgit config --global user.name den1barodsfclaerclear>
2021-12-05 11:36:57 -0300

    finish homework 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -72,6 +72,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  return str1.length === str2.length;
   
 }
 
@@ -79,19 +80,19 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num > 90) {
-    return true
+  if (num < 90) {
+    return true;
   } else {
-    return false
+    return false;
   }
-  
+
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num < 50) {
+  if (num > 50) {
     return true
   } else {
     return false
