c328e1534956dca1749119964a91f3bf0654ca9f
Den1Baro <denise_baroffio@hotmail.comgit config --global user.name den1barodsfclaerclear>
2021-12-05 14:39:13 -0300

    finish homework 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,32 +28,32 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  return str
+return str; 
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  return (x + y);
+  return x + y;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return x - y 
+  return x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  return x * y 
+  return x * y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  return x / y
+return x % y;
 }
 
 function sonIguales(x, y) {
@@ -61,26 +61,6 @@ function sonIguales(x, y) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (x === y) {
-    return true
-  } else {
-
-  }
-  return false
-}
-
-function tienenMismaLongitud(str1, str2) {
-  // Devuelve "true" si las dos strings tienen la misma longitud
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  return str1.length === str2.length;
-  
-}
-
-function menosQueNoventa(num) {
-  // Devuelve "true" si el argumento de la función "num" es menor que noventa
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  if (num < 90) {
     return true;
   } else {
     return false;
@@ -88,15 +68,26 @@ function menosQueNoventa(num) {
 
 }
 
+function tienenMismaLongitud(str1, str2) {
+  // Devuelve "true" si las dos strings tienen la misma longitud
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+}
+
+function menosQueNoventa(num) {
+  // Devuelve "true" si el argumento de la función "num" es menor que noventa
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  
+
+}
+
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num > 50) {
-    return true
-  } else {
-    return false
-  }
+
 }
 
 function obtenerResto(x, y) {
@@ -110,6 +101,7 @@ function esPar(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   
+  
 }
 
 function esImpar(num) {
@@ -117,6 +109,7 @@ function esImpar(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   
+
 }
 
 function elevarAlCuadrado(num) {
